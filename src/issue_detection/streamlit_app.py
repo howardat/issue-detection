a9755d8ad799f31b7be849d7a300af70ba8b7d5f
howardat <olzhas.nurzhanovich.kakimzhanov@gmail.com>
2026-01-17 18:43:25 +0500

    fixup! Merge pull request #11 from howardat/10-debug-organisation-id-extraction

--- src/issue_detection/streamlit_app.py
+++ src/issue_detection/streamlit_app.py
@@ -1,15 +1,17 @@
 import copy
 import streamlit as st
 import re
+import plotly.express as px
 
 from fetch_data import get_reviews
+from bar_chart import bar_chart
 from tag_generation import batch_processing
 
 st.title('2GIS Organisation Reviews')
 
 url = st.text_input('Enter 2GIS URL:')
 
-organisation_id = re.search(r'(/firm/\d{15,17})', url).group(1) if url else ''
+organisation_id = re.search(r'/firm/(\d{15,17})', url).group(1) if url else ''
 st.write(f'Organisation ID: {organisation_id}')
 
 if organisation_id:
@@ -20,6 +22,18 @@ if organisation_id:
     for i, item in enumerate(data_with_tags):
         item['tags'] = tags[i]
 
-    st.json(data_with_tags)
+    chart_data = bar_chart(data_with_tags)
+    chart_data = chart_data.sort_values('month')
 
-    st.bar_chart(data_with_tags, x='date_created', y='tags')
+    fig = px.bar(
+        chart_data, 
+        x='month', 
+        y='review_count', 
+        color='tags',
+        title="Negative Reviews",
+        barmode='stack'
+    )
+    fig.update_xaxes(categoryorder='category ascending')
+    st.plotly_chart(fig, use_container_width=True)
+
+    st.json(data_with_tags)
